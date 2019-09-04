3a5f3dc0d777b7862c751602222f04b34fc58ea2
Dmitry Ivanov <dmitryitdev@gmail.com>
2019-09-04 15:57:22 +0400

    Внесены изменения в cmarty

--- presentation/application.php
+++ presentation/application.php
@@ -6,3 +6,16 @@ require_once SMARTY_DIR . 'Smarty.class.php';
 Класс, расширяющий smarty, 
 используется для обработки и отображения файлов smarty
 */
+class Application extends Smarty
+{
+    //Конструктор класса
+    public function __construct() {
+        //Вызов конструктора smarty
+        parent::Smarty();
+        
+        //Меняем папки шаблонов по умолчанию
+        $this->template_dir = TEMPLATE_DIR;
+        $this->compile_dir  = COMPILE_DIR;
+        $this->config_dir   = CONFIG_DIR;
+    }
+}
